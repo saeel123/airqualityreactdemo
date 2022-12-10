74ae1f83c30de0f82cf2d19ca68f2b546c8aacaf
saeel <saeelmacman123@gmail.com>
2022-12-11 01:05:56 +0530

    added API response

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import leaflet from "leaflet";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import {
+  getDefaultObservationStation,
+  getObservationStationList,
+} from "../Services/dataFetchServices";
 require("highcharts/modules/exporting")(Highcharts);
+
 let mymap;
 let tdWmsPm2Layer;
 let tdWmsAODLayer;
@@ -13,7 +18,9 @@ function Auth() {
   }, []);
 
   const map = () => {
-    mymap = leaflet.map("map").setView([15.87, 100.9925], 5);
+    mymap = leaflet
+      .map("map", { minZoom: 5, maxZoom: 15 })
+      .setView([15.87, 100.9925], 5);
 
     leaflet
       .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -87,8 +94,6 @@ function Auth() {
   };
 
   const removeWmsPm2Layer = () => {
-    // mymap.removeLayer(tdWmsPm2Layer);
-    // tdWmsPm2Layer = null;
     console.log(tdWmsPm2Layer);
 
     let hasLayerd = mymap.hasLayer(tdWmsPm2Layer);
@@ -193,6 +198,54 @@ function Auth() {
     });
   };
 
+  const [stationOne, setstationOne] = useState("");
+  const [stationTwo, setstationTwo] = useState("");
+  const [stationThree, setstationThree] = useState("");
+  const [stationFour, setstationFour] = useState("");
+
+  const setDefaultObservationStation = () => {
+    let typeName = "pm";
+    let startDate = "2022-12-09";
+    let endDate = "2022-12-10";
+    let currentTimeHrMin = "22-14";
+
+    getDefaultObservationStation(typeName, startDate, endDate, currentTimeHrMin)
+      .then((res) => {
+        console.log(res.defaultStation);
+        let defaultStationArray = res.defaultStation;
+        setstationOne(defaultStationArray[0]);
+        setstationTwo(defaultStationArray[1]);
+        setstationThree(defaultStationArray[2]);
+        setstationFour(defaultStationArray[3]);
+      })
+      .catch((err) => {
+        console.log("axios err=", err);
+      });
+
+    return () => {};
+  };
+
+  const [observationStations, setObservationStations] = useState([]);
+  const getObservationStationsList = () => {
+    let startDate;
+    let endDate;
+    getObservationStationList(startDate, endDate)
+      .then((res) => {
+        console.log(res.data);
+        setObservationStations(res.data);
+      })
+      .catch((err) => {
+        console.log("axios err=", err);
+      });
+
+    return () => {};
+  };
+
+  useEffect(() => {
+    getObservationStationsList();
+    setDefaultObservationStation();
+  }, []);
+
   return (
     <div>
       {/* <!-- Sidebar --> */}
@@ -222,52 +275,39 @@ function Auth() {
           <div class="spacer-margin"></div>
           <div class="form-group">
             <label class="group-labels">Select Stations</label>
-            <select class="form-control">
-              <option>Rangoon</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
-              <option>Rangoon</option>
+            <select value={stationOne} class="form-control">
+              {observationStations.map((option, index) => (
+                <option value={option.value} key={index}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
           <div class="form-group">
-            <select class="form-control">
-              <option>New Delhi</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
+            <select value={stationTwo} class="form-control">
+              {observationStations.map((option, index) => (
+                <option value={option.value} key={index}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
           <div class="form-group">
-            <select class="form-control">
-              <option>Kolkata</option>
-              <option>Dhaka</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
+            <select value={stationThree} class="form-control">
+              {observationStations.map((option, index) => (
+                <option value={option.value} key={index}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
           <div class="form-group">
-            <select class="form-control">
-              <option>Islamabad</option>
-              <option>Embassy Kathmandu</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
+            <select value={stationFour} class="form-control">
+              {observationStations.map((option, index) => (
+                <option value={option.value} key={index}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
           <div class="spacer-margin"></div>
@@ -281,224 +321,6 @@ function Auth() {
             data-toggle="modal"
             data-target="#chartsModal"
           >
-            Display charts
-          </button>
-        </form>
-        <form class="archive-filter filter-form">
-          <div class="form-group">
-            <label class="group-labels">Select By</label>
-            <select class="form-control mb-0">
-              <option>Parameter</option>
-              <option>Location</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Pollutants</label>
-            <select class="form-control mb-3">
-              <option>PM / Ground Observation-PM2.5 (AirNow)</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Select Stations</label>
-            <select class="form-control">
-              <option>Rangoon</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <select class="form-control">
-              <option>Embassy Kathmandu</option>
-              <option>Dhaka</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <select class="form-control">
-              <option>Phora Durbar Kathmandu</option>
-              <option>Dhaka</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <select class="form-control">
-              <option>Dhaka</option>
-              <option>Embassy Kathmandu</option>
-              <option>Islamabad</option>
-              <option>Kabul</option>
-              <option>Kolkata</option>
-              <option>Lahore</option>
-              <option>New Delhi</option>
-              <option>Phora Durbar Kathmandu</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <button type="submit" class="btn btn-primary w-100 mb-4">
-            Compute
-          </button>
-          <button type="submit" class="btn btn-danger w-100">
-            Display charts
-          </button>
-        </form>
-        <form class="emission-filter filter-form">
-          <div class="form-group">
-            <label class="group-labels">Inventory</label>
-            <select class="form-control mb-0">
-              <option>REAS</option>
-              <option>GAINS</option>
-              <option>EDGAR</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Pollutants</label>
-            <select class="form-control mb-3">
-              <option>PM25</option>
-              <option>BC</option>
-              <option>CO</option>
-              <option>NOx</option>
-              <option>OC</option>
-              <option>PM10</option>
-              <option>PM25</option>
-              <option>SO2</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Sectors</label>
-            <select class="form-control">
-              <option>Total</option>
-              <option>Residential</option>
-              <option>Industry</option>
-              <option>Transport</option>
-              <option>Energy</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Year</label>
-            <select class="form-control">
-              <option>2015</option>
-              <option>1990</option>
-              <option>1995</option>
-              <option>2000</option>
-              <option>2005</option>
-              <option>2010</option>
-              <option>2015</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <button type="submit" class="btn btn-danger w-100 mt-4">
-            Display charts
-          </button>
-        </form>
-        <form class="forecast-filter filter-form">
-          <div class="form-group">
-            <label class="group-labels">Select Parameter</label>
-            <select class="form-control mb-0">
-              <option>Parameter</option>
-              <option>Location</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Pollutants</label>
-            <select class="form-control mb-3">
-              <option>PM / Model-PM2.5 (WRF-Chem)</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div class="spacer-margin"></div>
-          <div class="form-group">
-            <label class="group-labels">Select Stations</label>
-          </div>
-          <div class="radio-group d-flex align-items-center">
-            <div class="radio-group-prepend d-flex align-items-center">
-              <input type="radio" name="station-select" checked />
-              <select class="form-control">
-                <option selected>Major Cities</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="radio-group-append">
-              <select class="form-control">
-                <option selected>Kathmandu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
-          <div class="radio-group d-flex align-items-center">
-            <div class="radio-group-prepend d-flex align-items-center">
-              <input type="radio" name="station-select" />
-              <select class="form-control" disabled>
-                <option selected>Major Cities</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="radio-group-append">
-              <select class="form-control" disabled>
-                <option selected>Kathmandu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
-          <div class="radio-group d-flex align-items-center">
-            <div class="radio-group-prepend d-flex align-items-center">
-              <input type="radio" name="station-select" />
-              <select class="form-control" disabled>
-                <option selected>Major Cities</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="radio-group-append">
-              <select class="form-control" disabled>
-                <option selected>Kathmandu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
-          <div class="spacer-margin"></div>
-          <button type="submit" class="btn btn-primary w-100 mb-4">
-            Compute
-          </button>
-          <button type="submit" class="btn btn-danger w-100">
             Display charts
           </button>
         </form>
---
+++ src/Services/dataFetchServices.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+const SERVER_URL = "http://smog.icimod.org/apps";
+
+//let defaultobservationstation = `${SERVER_URL}/airquality/defaultobservationstation/?typeName=${typeName}&startDate=${startDate}&endDate=${endDate}-${currentTimeHrMin}`;
+let defaultobservationstation = `/data/defaultobservationstation.json`;
+export const getDefaultObservationStation = (
+  typeName,
+  startDate,
+  endDate,
+  currentTimeHrMin
+) => {
+  return new Promise((resolve, reject) => {
+    try {
+      axios
+        .get(defaultobservationstation)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject("Error in getAccountDetials Axios!");
+        });
+    } catch (error) {
+      console.error(
+        "in billingPlanServices > getAccountDetails, Err===",
+        error
+      );
+      reject("SYSTEM_ERROR");
+    }
+  });
+};
+
+//let getStationListUrl = `http://smog.icimod.org/apps/airquality/getGeoJsonForOneSatation/?ModelClass=UsEmbassyPm&ModelClassDataList=UsEmbassyDataList&typeName=pm&StartDate=${startDate}&EndDate=${endDate}&rid=0`;
+let getStationListUrl = "/data/stationList.json";
+export const getObservationStationList = (startDate, endDate) => {
+  return new Promise((resolve, reject) => {
+    try {
+      axios
+        .get(getStationListUrl)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject("Error in getAccountDetials Axios!");
+        });
+    } catch (error) {
+      console.error(
+        "in billingPlanServices > getAccountDetails, Err===",
+        error
+      );
+      reject("SYSTEM_ERROR");
+    }
+  });
+};
